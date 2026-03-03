c59ddafca20569f4557d863a749a9dad01299477
jbc9107 <jackson9107@hotmail.com>
2026-03-03 09:08:58 -0500

    Remove unguarded updates from add_email

--- server/sql/add_email.sql
+++ server/sql/add_email.sql
@@ -3,11 +3,6 @@ BEGIN
   IF to_regclass('public.users') IS NOT NULL THEN
     EXECUTE 'ALTER TABLE public.users ADD COLUMN IF NOT EXISTS email TEXT UNIQUE';
     EXECUTE 'UPDATE public.users SET email = CONCAT(username, ''@example.com'') WHERE email IS NULL';
+    EXECUTE 'ALTER TABLE public.users ALTER COLUMN email SET NOT NULL';
   END IF;
 END $$;
-
-UPDATE users SET email = CONCAT(username, '@example.com')
-WHERE email IS NULL;
-
-ALTER TABLE users
-ALTER COLUMN email SET NOT NULL;
